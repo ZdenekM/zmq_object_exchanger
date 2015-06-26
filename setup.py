dd8bdfb4dbe6ebc870c46b67751bb399131da1ba
ZdenekM <imaterna@fit.vutbr.cz>
2015-06-26 14:52:40 +0200

    setup.py updated to provide long_description based on github readme.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(name='zmq_object_exchanger',
       version='0.1',
       description='Simple and lightweight framework for exchanging Python objects over network.',
@@ -13,4 +20,5 @@ setup(name='zmq_object_exchanger',
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
+      long_description=read_md('README.md'),
       zip_safe=False)
